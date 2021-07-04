01d55c1c5e164858fa5fb78892b2964ff33d8b27
Akshat Trivedi <akshattriv.iit@gmail.com>
2021-07-04 21:53:20 +0530

    feat: removed pug template and pug view engine

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+const homePage = fs.readFileSync(`${__dirname}/views/index.html`);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.end(homePage);
 });
 
 app.post("/saveData", async (req, res) => {
